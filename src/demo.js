8601a3741eb4f37d32a9c2e43a56687353ee5ce6
chujunfang <chujunfang@qfpay.com>
2018-02-07 16:05:59 +0800

    测试组建安装

---
+++ src/demo.js
@@ -0,0 +1,42 @@
+import Reach, { PureComponent } from 'react';
+
+class Demo extends PureComponent {
+    static defaultProps = {
+        title: 'this is a demo',
+        value: 0
+    };
+    constructor(props) {
+        super(props);
+        const { title, value } = props;
+        this.state = {
+            title,
+            value
+        }
+    }
+    add = () => {
+        this.setState({
+            value: this.state.value++
+        });
+    }
+    change = ev => {
+        this.setState({
+            value: ev.targe.value
+        });
+    }
+    componentWillReceiveProps(nextProps){
+        this.setState({
+            title: nextProps.title
+        });
+    }
+    render() {
+        return ( 
+            <div className = "container" >            
+            <h1> { this.state.title } </h1> 
+            <div className = "counter" > { this.state.value } </div> 
+            <input value = { this.state.value } onChange = { this.change }
+            /> 
+            <button onClick={this.add}>value++</button>
+            </div>
+        );
+    }
+}
